043ae75e665986392acc7931b6ccfccab075c76a
mustapher11 <mustapher1997@gmail.com>
2022-12-15 04:28:31 -0800

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks whether an alphabet is uppercase or lowercase
- *
+ * @c: Integer to be tested
  * Return: Either 1 for lowercase and 0 for uppercase
  */
 int _islower(int c)
