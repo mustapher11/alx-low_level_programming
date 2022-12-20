665928dd29f3f6d7ccf2cabb77d12b2154ef971c
mustapher11 <mustapher1997@gmail.com>
2022-12-20 11:20:39 -0800

    I can only go one way. I have not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * print_rev - Print the reverse of a string
@@ -7,5 +8,6 @@
  */
 void print_rev(char *s)
 {
-printf("%s\n", strrev(s));
+strrev(s);
+printf("%s\n", s);
 }
