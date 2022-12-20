30d7171c1b239d1e201bef5b2743903e28c031fd
mustapher11 <mustapher1997@gmail.com>
2022-12-20 11:08:00 -0800

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to standard output
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(int n);
+void _puts(char *str);
 
 #endif
