85cb40034fb44efba1ddf07861a56b5ada885660
mustapher11 <mustapher1997@gmail.com>
2022-12-20 10:55:05 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: String whose length is computed
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 int _putchar(int n);
 
 #endif
