0d89c21c2c6f88a291eae81022e5e15fa2c00f12
mustapher11 <mustapher1997@gmail.com>
2022-12-22 01:38:04 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - Converts lowercase letters of a string to upper
+ * @s: String whose lowercase letters are to be converted to upper
+ * Return: Uppercase letter string.
+ */
+char *string_toupper(char *s)
+{
+return (strupr(s));
+}
